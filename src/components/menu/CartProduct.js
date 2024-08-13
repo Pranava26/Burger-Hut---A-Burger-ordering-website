af41d1474cc1a48f36a1c1eb77503034d3177cbf
Pranava26 <pranavasai26@gmail.com>
2024-08-13 12:54:37 +0530

    Code formatted

--- src/components/menu/CartProduct.js
+++ src/components/menu/CartProduct.js
@@ -1,25 +1,25 @@
 import Trash from "@/components/icons/Trash";
 import Image from "next/image";
 
-export default function CartProduct({product, index, onRemove}) {
+export default function CartProduct({ product, index, onRemove }) {
     return (
         <div className="flex gap-4 border-b py-4 items-center">
-                            <div className="w-24">
-                                <Image src={'/burger.png'} width={240} height={240} alt="burger-image" />
-                            </div>
-                            <div className="grow">
-                                {product.name}
-                            </div>
-                            <div className="text-lg font-semibold">
-                            ₹{product.basePrice}
-                            </div>
-                            {onRemove && (
-                                <div className="ml-2">
-                                <button type="button" onClick={()=>onRemove(index)} className="p-2">
-                                    <Trash />
-                                </button>
-                            </div>
-                            )}    
-                        </div>
+            <div className="w-24">
+                <Image src={'/burger.png'} width={240} height={240} alt="burger-image" />
+            </div>
+            <div className="grow">
+                {product.name}
+            </div>
+            <div className="text-lg font-semibold">
+                ₹{product.basePrice}
+            </div>
+            {onRemove && (
+                <div className="ml-2">
+                    <button type="button" onClick={() => onRemove(index)} className="p-2">
+                        <Trash />
+                    </button>
+                </div>
+            )}
+        </div>
     );
 }
